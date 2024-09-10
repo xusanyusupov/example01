6fcd11f1545b1b3559cbb90ac0645bc5e916a651
xusanyusupov <yusupov.xusan.org@gmail.com>
2024-09-10 21:13:31 +0500

    Js example

---
+++ main.js
@@ -0,0 +1,90 @@
+// {
+//     //////Masala 01
+//     function noSpace(x){
+//     return x.replace(/\s+/g, '');
+//   }
+// }
+
+// {
+//     //////Masala 02
+//     /////SHART sonni ikkiga ko'paytish
+//     const doubleInteger = x => x * 2
+// }
+
+// {
+//    /////Masala 03
+//    function litres(time) {
+//     return Math.floor(time * 0.5);
+//   }
+// }
+
+// {
+//     //////Masala 04
+//     function getCount(str) {
+//         const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+//           let count = 0;
+
+//           for (let char of str) {
+//               if (vowels.includes(char)) {
+//                   count++;
+//               }
+//           }
+
+//           console.log(count);
+//           return count;
+//       }
+//       getCount('assalomu aleykum')
+// }
+
+// {
+//     //////Masala 05
+//     function highAndLow(numbers){
+//         const arr = numbers.split(' ').map(Number);
+//         const max = Math.max(...arr)
+//         const min = Math.min(...arr);
+//         return max + ' ' + min;
+//       }
+//     highAndLow(55, 1 ,5 -20) 
+// }
+
+// {
+//     //////Masala 06
+//     function filter_list(l) {
+//         return l.filter(item => typeof item === 'number');
+//      }
+// }
+
+// {
+//     //////Masala 07
+//     var isSquare = function(n){
+//         return n >= 0 && Math.sqrt(n) % 1 === 0;
+//      }
+// }
+
+// {
+//     //////Masala 08
+//     maps = x => x.map(e => e * 2);
+// }
+
+// {
+//     /////Masala 09
+//     const lovefunc = (petals1, petals2) => (petals1 % 2 !== petals2 % 2);
+// }
+
+// {
+//     /////Masala 10
+//     const digitize = num => num.toString().split('').reverse().map(Number);
+// }
+
+// {
+    //////Masala 11
+//     function repeatStr(n, s) {
+//         return s.repeat(n);
+//     }
+// }
+
+// {
+//     /////Masala 12
+//     const simpleMultiplication = number => number % 2 === 0 ? number * 8 : number * 9
+//     /////son toq bo'lsa 9 ka ko'paytrladi aks holatda 8ga
+// }
